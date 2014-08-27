31763c7c49f122777f36aaa680cba49dc7f6a0c1
Anthony Fennell <fennell4511@anthonys-imac.attlocal.net>
2014-08-26 23:16:26 -0500

    Initial Commit

---
+++ Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Anthony Fennell on 8/26/14.
+//  Copyright (c) 2014 Anthony Fennell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
